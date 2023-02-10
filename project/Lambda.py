eea226a6eb96dff15cace8d3e49a2eee38c066ee
DevikaLak <devika.mle@gmail.com>
2023-02-10 13:48:10 +0000

    Lambda progress 3

--- project/Lambda.py
+++ project/Lambda.py
@@ -47,7 +47,7 @@ ENDPOINT = "image-classification-2023-02-10-07-53-29-808"
 def lambda_handler(event, context):
 
     # Decode the image data
-    image = base64.b64decode(event["body"]["image_data"])
+    image = base64.b64decode(event['body']['image_data'])
 
     # Instantiate a Predictor
     predictor = sagemaker.predictor.Predictor(
@@ -62,18 +62,21 @@ def lambda_handler(event, context):
     inferences = predictor.predict(image)
     
     # We return the data back to the Step Function    
-    event["body"]["inferences"] = inferences.decode('utf-8')
+    inferences = json.loads(inferences.decode('utf-8'))
     return {
         'statusCode': 200,
-        'body': json.dumps(event)
+        'body': {
+            "image_data": event['body']['image_data'],
+            "s3_bucket": event['body']['s3_bucket'],
+            "s3_key": event['body']['s3_key'],
+            "inferences": inferences
+        }
     }
 
-#Lambda Function 3 (Not tested)
+#Lambda Function 3 (Tested)
 import json
 
-
-THRESHOLD = .93
-
+THRESHOLD = .9
 
 def lambda_handler(event, context):
     
@@ -92,5 +95,10 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps(event)
+        'body': {
+            "image_data": event['body']['image_data'],
+            "s3_bucket": event['body']['s3_bucket'],
+            "s3_key": event['body']['s3_key'],
+            "inferences": inferences
+        }
     }
